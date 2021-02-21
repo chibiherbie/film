c4aea7fd009f0a87d0b6e756d5a72945cf0933cc
Roman <bekkerrdm@gmail.com>
2021-02-21 12:28:13 +0700

    Bot v0.2
    -База данны, которая позволит ограничивать ответы

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 import config
 from aiogram import Bot, Dispatcher, executor, types
+from sqlite import DataBase
 import asyncio
 
 bot = Bot(token=config.TOKEN)
@@ -8,10 +9,22 @@ dp = Dispatcher(bot)
 
 logging.basicConfig(level=logging.INFO)
 
+bd = DataBase('data/user.db')
+bd.clear()
+
+
+@dp.message_handler(commands=['start'])
+async def hi(message: types.Message):
+    await message.answer('Привет! Добро пожаловать. Следи за игрой и успевай отправлять ответы')
+
 
 @dp.message_handler(content_types=['text'])
 async def said(message: types.Message):
-    await message.answer(message.text)
+    if not bd.check_id(message.from_user.id):
+        bd.new_user(message.from_user.id, message.text)
+        await message.answer('Ваш ответ засчитан')
+    else:
+        await message.answer('Вы уже ответили')
 
 
 if __name__ == '__main__':
---
+++ sqlite.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+class DataBase:
+    def __init__(self, file):
+        self.con = sqlite3.connect(file)
+        self.cur = self.con.cursor()
+
+    def clear(self):
+        self.cur.execute('''DELETE FROM game''')
+        self.con.commit()
+
+    def new_user(self, id, msg):
+        self.cur.execute('''INSERT INTO game(id_user, choice) VALUES (?, ?)''', (id, msg))
+        self.con.commit()
+
+    def check_id(self, id):
+        res = self.cur.execute('''SELECT id_user FROM game WHERE id_user = ?''', (id, )).fetchall()
+        return bool(len(res))
