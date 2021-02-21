26e8df17f0957be76a89515165cb91820589fabd
Roman <bekkerrdm@gmail.com>
2021-02-21 19:57:32 +0700

    st

--- main.py
+++
@@ -1,7 +0,0 @@
-from video_site import app
-
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
-
-    a = input()
-    print(a)
--- video_site.py
+++ video_site.py
@@ -4,6 +4,15 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder='data')
 
 
+def some():
+    print('sdffds')
+
+
 @app.route('/')
 def index(f='1.mp4'):
+    some()
     return render_template('index.html', file=str(f))
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
